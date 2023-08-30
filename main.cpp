621259721bf1d55eae898bf8aeae7d738c6dac64
amwnag <amwang36@gmail.com>
2023-08-30 22:34:02 +0000

    solution

--- main.cpp
+++ main.cpp
@@ -1,17 +1,46 @@
 #include "mbed.h"
+#include "mbed.h"
+#include "PinDetect.h"
 
-DigitalIn button(p22);
-DigitalOut red(p21);
+PinDetect incr(p24);
+PinDetect decr(p23);
 
+PwmOut boardLED(LED1);
+DigitalOut debugLED(LED4);
+
+float volatile pwmVal;
+
+void incrCallback(void) {
+    pwmVal = (pwmVal >= 1) ? 1 : (pwmVal + 0.1);
+    boardLED = pwmVal;
+    wait(0.1);
+}
+
+void decrCallback(void) {
+    pwmVal = (pwmVal <= 0) ? 0 : (pwmVal - 0.1);
+    boardLED = pwmVal;
+    wait(0.1);
+}
 
 int main() {
-    red = 0;
-    while(1) {
-        if (button) { // not pressed
-            red = 0;
-        } else {
-            red = 1;
-        }
-        wait(0.2);
+    pwmVal = 1;
+    boardLED = pwmVal;
+
+    incr.mode(PullUp);
+    decr.mode(PullUp);
+    wait(0.001); // delay for initial pullup
+
+    // interrupt callback function
+    incr.attach_deasserted(&incrCallback);
+    decr.attach_deasserted(&decrCallback);
+
+    // sampling freq
+    incr.setSampleFrequency();
+    decr.setSampleFrequency();
+
+    debugLED = 1;
+    while (1) {
+        debugLED = !debugLED;
+        wait(1);
     }
 }
