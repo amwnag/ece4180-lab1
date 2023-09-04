d03407b6f7a8622b77608c871994e487582540bb
amwnag <amwang36@gmail.com>
2023-09-04 23:30:46 +0000

    part 9

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "mbed.h"
-PwmOut myled(p21);
-AnalogIn mypotentiometer(p20);
+
+// The sinewave is created on this pin
+// https://os.mbed.com/handbook/AnalogOut
+AnalogOut aout(p18);
 
 int main()
 {
+    const double pi = 3.141592653589793238462;
+    double rads = 0.0;
+    double sample = 0;
+    
     while(1) {
-        myled = mypotentiometer;
-        wait(0.01);
+        // sinewave output
+        for (int i = 0; i < 360; i+=18) { // 20 samples per cycle
+            rads = (pi * i) / 180.0f;
+            sample = ((0.5f)*sin(rads + pi) + 0.5f);
+            aout.write(sample);
+        }
     }
 }
+ 
