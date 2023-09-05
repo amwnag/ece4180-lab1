8b2c53b98185dfbaf848c68e0ffb683cb79a3bc9
amwnag <amwang36@gmail.com>
2023-09-05 01:29:48 +0000

    copy pasted

--- main.cpp
+++ main.cpp
@@ -1,22 +1,2 @@
-#include "mbed.h"
-
-// The sinewave is created on this pin
-// https://os.mbed.com/handbook/AnalogOut
-AnalogOut aout(p18);
-
-int main()
-{
-    const double pi = 3.141592653589793238462;
-    double rads = 0.0;
-    double sample = 0;
-    
-    while(1) {
-        // sinewave output
-        for (int i = 0; i < 360; i+=18) { // 20 samples per cycle
-            rads = (pi * i) / 180.0f;
-            sample = ((0.5f)*sin(rads + pi) + 0.5f);
-            aout.write(sample);
-        }
-    }
-}
- 
+//DMA to DAC sine wave demo - p18
+#include "example4.h"
